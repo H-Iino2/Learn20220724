10fca51f04c767da69c84991ca229f4eaa9c8f44
H-iino2 <hiro110pg@gmail.com>
2022-08-02 23:43:23 +0900

    　購入金額to

--- top.js
+++ top.js
@@ -1,8 +1,29 @@
 document.getElementById("money-button").onclick = function() {
 
-    
-    document.getElementById("left-result").insertAdjacentHTML('beforeend','<p>'+pay+'</p>');
-   console.log(pay);
+    //購入者を定義
+    let purchaser = document.getElementById("purchaser").value;
+
+    //購入品を定義
+    let goods;
+    goods = document.getElementById("goods").value;
+
+    //購入金額を定義
+    let pay = document.getElementById("pay").value
+    numPay = Number(pay);
+
+    let APay;
+    let BPay;
+
+
+    if (purchaser === "A"){
+      document.getElementById("left-result-goods").insertAdjacentHTML('beforeend','<p>'+goods+ '</p>');
+      document.getElementById("left-result-pay").insertAdjacentHTML('beforeend','<p>'+numPay+ '円<p>');
+    } else if(purchaser === "B"){
+      document.getElementById("right-result-goods").insertAdjacentHTML('beforeend','<p>'+goods+ '</p>');
+      document.getElementById("right-result-pay").insertAdjacentHTML('beforeend','<p>'+numPay+ '円</p>');
+    } 
+
+
 
   }
 
