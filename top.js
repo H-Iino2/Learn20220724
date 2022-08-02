d3246e527e49bbe4e12e8a11022768a6d6856f94
H-iino2 <hiro110pg@gmail.com>
2022-08-02 20:38:03 +0900

    20220802

---
+++ top.js
@@ -0,0 +1,8 @@
+document.getElementById("money-button").onclick = function() {
+
+    
+    document.getElementById("left-result").insertAdjacentHTML('beforeend','<p>'+pay+'</p>');
+   console.log(pay);
+
+  }
+
