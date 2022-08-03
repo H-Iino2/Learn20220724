b15eb28534ce03df122579567551a47d03008cf2
H-iino2 <hiro110pg@gmail.com>
2022-08-03 21:15:25 +0900

    機能ha

--- top.js
+++ top.js
@@ -1,3 +1,7 @@
+let APay = null;
+let BPay = null;
+let finPay = null;
+
 document.getElementById("money-button").onclick = function() {
 
     //購入者を定義
@@ -11,19 +15,41 @@ document.getElementById("money-button").onclick = function() {
     let pay = document.getElementById("pay").value
     numPay = Number(pay);
 
-    let APay;
-    let BPay;
+
 
 
     if (purchaser === "A"){
       document.getElementById("left-result-goods").insertAdjacentHTML('beforeend','<p>'+goods+ '</p>');
       document.getElementById("left-result-pay").insertAdjacentHTML('beforeend','<p>'+numPay+ '円<p>');
+      APay = APay + numPay;
+      document.getElementById("left-pay-merge").innerHTML=APay;
     } else if(purchaser === "B"){
       document.getElementById("right-result-goods").insertAdjacentHTML('beforeend','<p>'+goods+ '</p>');
       document.getElementById("right-result-pay").insertAdjacentHTML('beforeend','<p>'+numPay+ '円</p>');
+      BPay = BPay + numPay;
+      document.getElementById("right-pay-merge").innerHTML=BPay;
     } 
 
+    if (APay > BPay){
+      finPay = APay - BPay;
+
+      finMessage = 'AがBに' + finPay + '円払う'
+      document.getElementById("fin-pay").innerHTML = finMessage;
+    } else if (APay < BPay){
+      finPay = BPay - APay;
+
+      finMessage = 'BがAに' + finPay + '円払う'
+      document.getElementById("fin-pay").innerHTML = finMessage;
+    }
+
+    console.log(finMessage);
+
+
+    //入力フォームをリセット]
+    document.getElementById("goods").value = "";
+    document.getElementById("pay").value = "";
 
 
   }
+
 
